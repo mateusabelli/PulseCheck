112b65863bbe52a9fc1584ca3912b7eb11ee8ebf
Mateus Abelli <mateusabelli@gmail.com>
2026-03-02 11:24:35 -0300

    Update CLI to consume the new service

--- PulseCheck.Cli/Program.cs
+++ PulseCheck.Cli/Program.cs
@@ -1,13 +1,17 @@
-﻿using PulseCheck.Core.Platform;
+﻿using PulseCheck.Core.Domain;
+using PulseCheck.Core.Platform;
 using PulseCheck.Core.Services;
 
 var runner = new BashCommandRunner();
 var parser = new BashOutputParser();
+var thresholds = new StabilityThresholds(85.0f, 95.0f);
 
 var resourceReader = new ResourceReader(runner, parser);
 
-var (cpuUsage, memoryUsage) = resourceReader.ReadUsagePercent();
+var snapshot = resourceReader.ReadUsagePercent();
+var stability = SystemStabilityReader.GetCurrentState(snapshot, thresholds);
 
 Console.WriteLine("In use system resources");
-Console.WriteLine($"CPU: {cpuUsage}%");
-Console.WriteLine($"MEM: {memoryUsage}%");
+Console.WriteLine($"CPU: {snapshot.CpuUsagePercent}%");
+Console.WriteLine($"MEM: {snapshot.MemoryUsagePercent}%");
+Console.WriteLine($"The system is {stability}");
